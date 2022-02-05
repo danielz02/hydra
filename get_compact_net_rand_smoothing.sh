1c6b5821fcc6b5104b277016d61b5be8e8b45978
danielz <31590926+danielz02@users.noreply.github.com>
2022-02-05 13:12:20 -0600

    Change the driver script for HYDRA baseline to conform with new commandline argument changes

--- get_compact_net_rand_smoothing.sh
+++ get_compact_net_rand_smoothing.sh
@@ -8,36 +8,38 @@ pretrain_prune_finetune_semisup() {
 
     # pre-training
     python train.py --is-semisup --exp-name $1 --arch $2 --exp-mode pretrain --configs configs/configs.yml \
-    --trainer $3 --val_method $4 --gpu $5 --k 1.0 --save-dense --dataset CIFAR10 --noise-std 0.25 ;
+    --trainer $3 --val_method $4 --gpu $5 --k 1.0 --save-dense --dataset CIFAR10 --noise-std 0.25 --num-models 1 \
+    --start-epoch 0;
 
     # pruning
     python train.py --is-semisup --exp-name $1 --arch $2 --exp-mode prune --configs configs/configs.yml \
     --trainer $3 --val_method $4 --gpu $5 --k $6 --save-dense --scaled-score-init \
     --source-net ./trained_models/$1/pretrain/latest_exp/checkpoint/checkpoint.pth.tar \
-    --epochs $7 --dataset CIFAR10 --noise-std 0.25 ;
+    --epochs $7 --dataset CIFAR10 --noise-std 0.25 --num-models 1 --start-epoch 0;
 
     # finetuning
     python train.py --is-semisup --exp-name $1 --arch $2 --exp-mode finetune --configs configs/configs.yml \
     --trainer $3 --val_method $4 --gpu $5 --k $6 --save-dense --dataset CIFAR10 --noise-std 0.25 \
-    --source-net ./trained_models/$1/prune/latest_exp/checkpoint/checkpoint.pth.tar --lr 0.01 ;
+    --source-net ./trained_models/$1/prune/latest_exp/checkpoint/checkpoint.pth.tar --lr 0.01 --num-models 1 \
+    --start-epoch 0;
 
     # weight base pruning
-    python train.py --is-semisup --exp-name $1"_weight_based_pruning" --arch $2 --exp-mode finetune --configs configs/configs.yml \
-    --trainer $3 --val_method $4 --gpu $5 --k $6 --save-dense --scaled-score-init --dataset CIFAR10 --noise-std 0.25 \
-    --source-net ./trained_models/$1/pretrain/latest_exp/checkpoint/checkpoint.pth.tar --lr 0.01 ;
+#    python train.py --is-semisup --exp-name $1"_weight_based_pruning" --arch $2 --exp-mode finetune --configs configs/configs.yml \
+#    --trainer $3 --val_method $4 --gpu $5 --k $6 --save-dense --scaled-score-init --dataset CIFAR10 --noise-std 0.25 \
+#    --source-net ./trained_models/$1/pretrain/latest_exp/checkpoint/checkpoint.pth.tar --lr 0.01 --num-models 1;
 }
 
 
-arch="vgg16_bn" 
-(
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.1-cifar10"  $arch "smooth"   "smooth"  "0"   0.1  20 &
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.05-cifar10"  $arch "smooth"   "smooth"  "1"   0.05  20 &
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.01-cifar10"  $arch  "smooth"   "smooth"  "2"   0.01  20 ;
-); 
+#arch="vgg16_bn"
+#(
+#    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.1-cifar10"  $arch "smooth"   "smooth"  "0"   0.1  20 &
+#    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.05-cifar10"  $arch "smooth"   "smooth"  "1"   0.05  20 &
+#    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.01-cifar10"  $arch  "smooth"   "smooth"  "2"   0.01  20 ;
+#);
 
 arch="wrn_28_4"
 (
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.1-cifar10"  $arch "smooth"   "smooth"  "0"   0.1  20 &
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.05-cifar10"  $arch "smooth"   "smooth"  "1"   0.05  20 &
-    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.01-cifar10"  $arch  "smooth"   "smooth"  "2"   0.01  20 ;
+    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.1-cifar10-reproduce"  $arch "smooth"   "smooth"  "0,1,2,3"   0.1  20 &
+    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.05-cifar10-reproduce"  $arch "smooth"   "smooth"  "0,1,2,3"   0.05  20 &
+    pretrain_prune_finetune_semisup  "semisup-$arch-trainer_smooth-k_0.01-cifar10-reproduce"  $arch  "smooth"   "smooth"  "0,1,2,3"   0.01  20 ;
 );
