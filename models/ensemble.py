51e3659c852de7ecef135315c4474a5764dbf0e2
danielz02 <31590926+danielz02@users.noreply.github.com>
2022-03-02 16:05:52 -0600

    Support Bézier Curve subspace training with DRT loss

--- models/ensemble.py
+++ models/ensemble.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,3 +19,49 @@ class Ensemble(nn.Module):
             return output
         else:
             return self.models[0](x)
+
+
+class BezierCurve(Ensemble):
+    def __init__(self, subspace_model, is_layerwise: bool):
+        models = [BezierEndPoint(subspace_model, i) for i in range(3)]
+        super().__init__(models)
+
+        self.layerwise = is_layerwise
+        self.subspace_model = subspace_model
+
+    def __sample_subnet(self, **kwargs):
+        fix_alpha = kwargs.get("fixed_alpha")
+        if fix_alpha is None and not self.training and not hasattr(self, "fixed_alpha"):
+            fix_alpha = 0.5
+        elif hasattr(self, "fixed_alpha"):
+            fix_alpha = self.fixed_alpha
+
+        if fix_alpha:
+            print(f"Using fixed alpha {fix_alpha}")
+
+        alpha = np.random.uniform(0, 1) if not fix_alpha else fix_alpha
+        for m in self.subspace_model.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.BatchNorm2d):
+                setattr(m, f"alpha", alpha)
+            if self.layerwise:
+                alpha = np.random.uniform(0, 1) if not fix_alpha else fix_alpha
+
+    def forward(self, x):
+        self.__sample_subnet()
+        return self.subspace_model(x)
+
+
+class BezierEndPoint(nn.Module):
+    def __init__(self, bezier_subspace, idx: int):
+        assert idx in [0, 1, 2]
+        super().__init__()
+
+        self.point_idx = idx
+        self.bezier_subspace = bezier_subspace
+
+    def forward(self, x):
+        setattr(self.bezier_subspace, f"w{self.point_idx}", True)
+        out = self.bezier_subspace(x)
+        delattr(self.bezier_subspace, f"w{self.point_idx}")
+
+        return out
--- utils/drt.py
+++ utils/drt.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -10,7 +12,7 @@ import torch.nn.functional as F
 import sys
 import os
 
-from models.ensemble import Ensemble
+from models.ensemble import Ensemble, BezierCurve
 from utils.model import prepare_model
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -18,7 +20,7 @@ parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.append(parentdir)
 
 from utils.smoothadv import Attacker
-from utils.utils_ensemble import AverageMeter, accuracy, test, copy_code, requires_grad_
+from utils.utils_ensemble import AverageMeter, accuracy, test, copy_code, requires_grad_, get_stats
 
 
 def get_minibatches(batch, num_batches):
@@ -30,15 +32,20 @@ def get_minibatches(batch, num_batches):
         yield X[i * batch_size: (i + 1) * batch_size], y[i * batch_size: (i + 1) * batch_size]
 
 
-def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: Ensemble, criterion, optimizer: Optimizer, epoch: int, noise_sd: float,
-                attacker: Attacker, device: torch.device, writer=None):
+def DRT_Trainer(
+        args, train_loader: DataLoader, sm_loader: DataLoader, models: Ensemble | BezierCurve, criterion,
+        optimizer: Optimizer, epoch: int, noise_sd: float, attacker: Attacker, device: torch.device, writer=None
+):
     batch_time = AverageMeter("batch_time")
     data_time = AverageMeter("data_time")
     losses = AverageMeter("losses")
     losses_lhs = AverageMeter("loss_lhs")
     losses_rhs = AverageMeter("loss_rhs")
+    cosine = AverageMeter("Cosine", ":6.2f")
+    l2 = AverageMeter("L2", ":6.2f")
     top1 = AverageMeter("top1")
     top5 = AverageMeter("top5")
+
     end = time.time()
 
     models.train()
@@ -54,6 +61,7 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                 torch.cat([d[1] for d in batch], 0),
             )
 
+        # TODO: Bug fix - use same sample of models in  benign loss and regularizer
         mini_batches = get_minibatches(batch, args.num_noise_vec)
         for inputs, targets in mini_batches:
             inputs, targets = inputs.to(device), targets.to(device)
@@ -63,6 +71,8 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                       for _ in range(args.num_noise_vec)]
 
             if args.adv_training:
+                if isinstance(models, BezierCurve):
+                    raise NotImplementedError("SmoothAdv is not supported with self-ensemble!")
                 adv_x = []
                 for j in range(args.num_models):
                     requires_grad_(models.models[j], False)
@@ -84,18 +94,16 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
             targets = targets.repeat(args.num_noise_vec)
             loss_std = 0
 
-            for j in range(args.num_models):
-                if args.adv_training:
-                    logits = models.models[j](adv_input[j])
-                else:
-                    logits = models.models[j](noisy_input)
-                loss_std += criterion(logits, targets)
-                if args.trades_loss:
-                    loss_robust = (1.0 / len(inputs)) * nn.KLDivLoss(size_average=False)(
-                        F.log_softmax(models.models[j](noisy_input), dim=1),
-                        F.softmax(logits, dim=1),
-                    )
-                    loss_std += loss_robust
+            if isinstance(models, BezierCurve):
+                logits = models(noisy_input)
+                loss_std = criterion(logits, targets)
+            else:
+                for j in range(args.num_models):
+                    if args.adv_training:
+                        logits = models.models[j](adv_input[j])
+                    else:
+                        logits = models.models[j](noisy_input)
+                    loss_std += criterion(logits, targets)
 
             rhsloss, rcount = 0, 0
             pred = []
@@ -105,7 +113,10 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                     cur_input = adv_input[j]
                 else:
                     cur_input = noisy_input
-                output = models.models[j](cur_input)
+                if isinstance(models, BezierCurve):
+                    output = models(cur_input)
+                else:
+                    output = models.models[j](cur_input)
                 _, predicted = output.max(1)
                 pred.append(predicted == targets)
                 predicted = softma(output.sort()[0])
@@ -123,7 +134,7 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                 rhsloss += torch.sum(flg * predicted)
                 rcount += torch.sum(flg)
 
-            rhsloss /= max(rcount, 1.)
+            rhsloss /= max(rcount, 1)
 
             lhsloss, N = 0, 0
             mse = nn.MSELoss(reduce=False)
@@ -134,7 +145,7 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                     lhsloss += torch.sum(grad_norm * flg)
                     N += torch.sum(flg)
 
-            lhsloss /= max(N, 1.)
+            lhsloss /= max(N, 1)
 
             losses_lhs.update(lhsloss.item(), batch_size)
 
@@ -145,6 +156,12 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
             losses_rhs.update(rhsloss.item(), batch_size)
             top1.update(acc1.item(), batch_size)
             top5.update(acc5.item(), batch_size)
+
+            if args.layer_type == "curve":
+                weight_cosine, weight_l2 = get_stats(models, args)
+                cosine.update(weight_cosine, batch_size)
+                l2.update(weight_l2, batch_size)
+
             # compute gradient and do SGD step
             optimizer.zero_grad()
             loss.backward()
@@ -159,12 +176,14 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
                 'Epoch: [{0}][{1}/{2}]\t'
                 'Time {batch_time.avg:.3f}\t'
                 'Data {data_time.avg:.3f}\t'
+                'Weight-L2 {l2.avg:.4f}\t'
+                'Weight-Cosine {cosine.avg:.4f}\t'
                 'Loss-Std {loss.avg:.4f}\t'
                 'Loss-GD {lhs.avg:.4f}\t'
                 'Loss-CM {rhs.avg:.4f}\t'
                 'Acc@1 {top1.avg:.3f}\t'
                 'Acc@5 {top5.avg:.3f}'.format(
-                    epoch, i, len(train_loader), batch_time=batch_time,
+                    epoch, i, len(train_loader), batch_time=batch_time, l2=l2, cosine=cosine,
                     data_time=data_time, loss=losses, lhs=losses_lhs, rhs=losses_rhs, top1=top1, top5=top5
                 )
             )
@@ -172,6 +191,8 @@ def DRT_Trainer(args, train_loader: DataLoader, sm_loader: DataLoader, models: E
     writer.add_scalar('loss/train', losses.avg, epoch)
     writer.add_scalar('loss/lhs', losses_lhs.avg, epoch)
     writer.add_scalar('loss/rhs', losses_rhs.avg, epoch)
+    writer.add_scalar('weight/l2', l2.avg, epoch)
+    writer.add_scalar('weight/cosine', cosine.avg, epoch)
     writer.add_scalar('batch_time', batch_time.avg, epoch)
     writer.add_scalar('accuracy/train@1', top1.avg, epoch)
     writer.add_scalar('accuracy/train@5', top5.avg, epoch)
